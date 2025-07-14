dc93d1650ee5298246b6c23df0c6fc00128ec709
TheKiryuKha <Kirillzuyeu@gmail.com>
2025-07-14 11:35:06 +0300

    chore: add property-read annotations to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property-read int $id
+ * @property-read string $title
+ * @property-read string $description
+ * @property-read string $link
+ * @property-read CarbonImmutable $created_at
+ * @property-read CarbonImmutable $updated_at
+ */
 final class Project extends Model
 {
     /** @use HasFactory<\Database\Factories\ProjectFactory> */
